e195ef4160e4f7c2cd8d4ff32a47ed35fe16256c
Camilo Game <tenorio2003@gmail.com>
2025-02-25 15:42:04 -0500

    adding_hero

--- LabWeb01/src/App.jsx
+++ LabWeb01/src/App.jsx
@@ -1,7 +1,7 @@
 import Card from './components/Card/card'
 import Hero from './components/Hero/Hero'
 import CardsContainer from './components/CardsContainer/CardsContainer'
-import MyHeader from './components/MyHeader/Header'
+import Navbar from './components/MyHeader/Navbar'
 import gameInfo from './db/db'
 import './App.css'
 
@@ -11,7 +11,8 @@ function App() {
   return (
     <>
 
-    <MyHeader></MyHeader>
+    <Navbar></Navbar>
+
     <Hero image={"https://images.ctfassets.net/u73tyf0fa8v1/ILvL75mZNtImSfKerDXnp/d896e2442d635c3265f977d37f09f44a/Web_Header_2540x13002.jpg?w=1920&q=75"}></Hero>  
 
     <CardsContainer cardsInfo={gameInfo}></CardsContainer>
---
+++ LabWeb01/src/components/MyHeader/Navbar.jsx
@@ -0,0 +1,30 @@
+import MyButton from "../MyButton/MyButton"
+import './Navbar.css'
+
+const Navbar = () => {
+    return(
+        <div id="navbar-container">
+
+            <div id="left-side">
+                <img id="paradox-logo" src="https://www.paradoxinteractive.com/images/logo/logo-platypus-white.svg"></img>
+                <img id="ck-logo" src="https://images.ctfassets.net/u73tyf0fa8v1/78eMnPuCsEMpHS3aZoM54i/48df13a2896e717a8c050d7f8f0180fa/CK3_Logo_Horizontal_White.png?w=3840&q=75"></img>
+                <p className="navbar-text">ABOUT</p>
+                <p className="navbar-text">ADD-ONS</p>
+                <p className="navbar-text">NEWS</p>
+                <p className="navbar-text">ON CONSOLE</p>
+                <p className="navbar-text">LEGENDS OF CRUSADER KINGS III</p>
+
+            </div>
+                
+            <div id="right-side">
+                <MyButton text={"BUY NOW"}></MyButton>
+                <img id="shopping-cart"></img>
+                <p className="navbar-text">LOG IN</p>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Navbar
