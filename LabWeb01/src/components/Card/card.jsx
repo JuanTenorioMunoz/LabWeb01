70fd3260e5915b8a318f3c06f92d6255f3bd15c5
Camilo Game <tenorio2003@gmail.com>
2025-02-25 18:10:21 -0500

    fix_descriptionManagement

--- LabWeb01/src/components/Card/card.jsx
+++ LabWeb01/src/components/Card/card.jsx
@@ -1,11 +1,18 @@
 import './card.css'
 
-const Card = ({slogan, availability, image}) => {
+const Card = ({slogan, description, image}) => {
+
+    const descriptionArray = description
+
     return (
     <div className="card-info">
-        <img className="card-image"></img>
+        <img className="card-image" src={image}></img>
         <h1 className="slogan">{slogan}</h1>
-        <p className="description">{description}</p>
+        {descriptionArray.map((paragraph,index)=> {
+            return(
+                <p key={index} className='description'>{paragraph}</p>
+            )
+        })}
     </div>
     )
     
--- LabWeb01/src/db/db.js
+++ LabWeb01/src/db/db.js
@@ -1,15 +1,18 @@
 const gameInfo = [
     {   
         image: "",
-        availability: "wishlist now!",
         slogan: "Lead A Great National Rebirth",
-        description: "Resist the demands of foreign great powers and claim your own place on the world stage, engaging in diplomacy and warfare to lead a great national rebirth in the vast lands between the Euphrates and the Gulf of Bengal.",
+        description: [
+            "Take command of your house and expand your dynasty through a meticulously researched Middle Ages. Begin in 867 or 1066 and claim lands, titles, and vassals to secure a realm worthy of your royal blood. Your death is only a footnote as your lineage continues with new playable heirs, either planned… or not.",
+            "Discover a sprawling simulated world teeming with peasants and knights, courtiers, spies, knaves and jesters, and secret love affairs. An extensive cast of historical characters can be romanced, betrayed, executed, or subtly influenced.",
+            "Explore a vast medieval map stretching from the snowswept Nordic lands to the Horn of Africa, and the British Isles in the west to the exotic riches of Burma in the east. Claim, conquer, and rule thousands of unique counties, duchies, kingdoms, and empires",
+            ]
     },
     {   
         image: "",
-        availability: "wishlist now!",
         slogan: "Content Creator Pack Artic Attire",
-        description: "Brave the chill of far northern lands in style with the Arctic Attire Content Creator Pack, made in collaboration with the established Crusader Kings III modder Aj.",
+        description: ["Brave the chill of far northern lands in style with the Arctic Attire Content Creator Pack, made in collaboration with the established Crusader Kings III modder Aj.",
+        "Groom your heir with the appropriate guardians or educate them yourself. If found wanting, marry them off or despatch them through other means."],
     }
 ]
 
