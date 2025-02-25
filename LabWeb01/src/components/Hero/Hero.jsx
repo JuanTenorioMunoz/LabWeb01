e911bdec4edd7707945553663d3376d99b8c5b76
Camilo Game <tenorio2003@gmail.com>
2025-02-25 17:31:57 -0500

    add_middleContainer in heroImage

--- LabWeb01/src/components/Hero/Hero.jsx
+++ LabWeb01/src/components/Hero/Hero.jsx
@@ -5,13 +5,15 @@ const Hero = ({image,logo,}) => {
     return(
         <div className="hero">
             <img id="hero-image" src={image}></img>
-            <img id="hero-logo" src="https://images.ctfassets.net/u73tyf0fa8v1/78eMnPuCsEMpHS3aZoM54i/48df13a2896e717a8c050d7f8f0180fa/CK3_Logo_Horizontal_White.png?w=3840&q=75"></img>
 
-            <div id="hero-buttons">
-                <MyButton text={"BUY NOW"}></MyButton>
-                <MyButton text={"WATCH TRAILER"}></MyButton>
+            <div id="middle-container">
+                <img id="hero-logo" src="https://images.ctfassets.net/u73tyf0fa8v1/78eMnPuCsEMpHS3aZoM54i/48df13a2896e717a8c050d7f8f0180fa/CK3_Logo_Horizontal_White.png?w=3840&q=75"></img>
+                <div id="hero-buttons">
+                    <MyButton text={"BUY NOW"}></MyButton>
+                    <MyButton text={"WATCH TRAILER"}></MyButton>
+                </div>
             </div>
-
+            
             <div id="gradient"></div>
         </div>
     )
