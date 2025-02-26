721745bfa6b292c66420ace123e3358fcc8b58d6
Camilo Game <tenorio2003@gmail.com>
2025-02-26 13:24:22 -0500

    add_workingFeaturette

--- LabWeb01/src/components/Featurette/Featurette.jsx
+++ LabWeb01/src/components/Featurette/Featurette.jsx
@@ -4,29 +4,29 @@ import MyButton from "../MyButton/MyButton"
 const Featurette = ({id, db}) => {
 
     const featurette = db.filter((item) => item.id === id);
-
-    featurette.map((featurette)=>{
-        return(
-        <div className="featurette-container">
-
-            <img className="featurette-banner" src={featurette.banner}></img>
-
-            <div className="featurette-text-container">
-                <h1 className="featurette-title">{featurette.title}</h1>
-                <h2 className="featurette-subtitle">{featurette.subtitle}</h2>
-                {featurette.description.map((paragraph)=>{
-                    return(
-                        <p className="featurette-paragraph">{paragraph}</p>
-                    )
-                })}
-
-                <MyButton text={featurette.buttonText}></MyButton>
-            </div>
-        </div>
-        )
-    })
-
     
+    if (featurette.length != 0) {
+        console.log("care " + featurette)
+
+        return (
+            <div className="featurette-container">
+                {featurette.map((feature, index) => (
+                    <div key={index} className="featurette">
+                        <img className="featurette-banner" src={feature.banner}/>
+                        <div className="featurette-text-container">
+                            <h1 className="featurette-title">{feature.title}</h1>
+                            <h2 className="featurette-subtitle">{feature.subtitle}</h2>
+                            {feature.description.map((paragraph, i) => (
+                                <p key={i} className="featurette-paragraph">{paragraph}</p>
+                            ))}
+                            <MyButton text={feature.buttonText} />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+
+    }
 }
 
 export default Featurette
