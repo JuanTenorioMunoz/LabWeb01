a576df5a2c5cafe45713dbfa70a5b431d76df99a
Camilo Game <tenorio2003@gmail.com>
2025-02-27 10:47:53 -0500

    add_svg

--- LabWeb01/src/components/MyButton/MyButton.jsx
+++ LabWeb01/src/components/MyButton/MyButton.jsx
@@ -5,7 +5,7 @@ const MyButton = ({text, type, color}) => {
     const setImage = () => {
         switch (type) {
           case "arrow":
-            return <img src="/arrow.png"/>;
+            return <img src="https://raw.githubusercontent.com/JuanTenorioMunoz/LabWeb01/8ef70bd7c24f086af7ea8342c7fa605a145f71c3/LabWeb01/src/assets/svg/arrow.svg"/>;
           case "buy":
             return <img src="https://raw.githubusercontent.com/JuanTenorioMunoz/LabWeb01/f7f83449c47f00b4c31da4757660285fc6381ab7/LabWeb01/src/assets/svg/cart.svg"/>;
           default:
